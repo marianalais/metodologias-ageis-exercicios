c4410e7c81c347887aa85d3a93218e81525fb890
mariana <marianalais.eng@gmail.com>
2023-06-09 21:14:25 -0300

    metodologia agil em grupo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 // EXERCÍCIO 01
-export function anoBissexto(parameter) {
-
+export function anoBissexto(ano) {
+  if (ano < 0) {
+    return "INVALID";
+  } else if (ano % 4 === 0 && (ano % 100 !== 0 || ano % 400 === 0)) {
+    return "TRUE";
+  } else {
+    return "FALSE";
+  }
 }
-
 
 // EXERCÍCIO 02
 export function pedraPapelTesoura() {
-
+  const opcoes = ["PEDRA", "PAPEL", "TESOURA"];
+  const indiceAleatorio = Math.floor(Math.random() * 3);
+  return opcoes[indiceAleatorio];
 }
 
 // EXERCÍCIO 03
-export function checkNumeroPrimo(parameter) {
-
+export function checkNumeroPrimo(number) {
+  if (number < 2) {
+    return "INVALID";
+  }
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return "FALSE";
+    }
+  }
+  return "TRUE";
 }
